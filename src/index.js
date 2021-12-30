bd1fd95ed9f3158d55cd2a7783fe76d55ebcabba
Buyaki <rittahbuyaki@gmail.com>
2021-12-30 19:06:46 +0300

    Add Meal images, title and Description

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ import './style.css'
 import {getMeals} from './apiRequest.js';
 
 const foodsBody= document.querySelector('.foods');
-// const foodDetails = document.querySelectorAll('.foodContent');
-// function foodPopUp(){
-
-// }
-// foodDetails.forEach((item)=> {
-//     item.addEventListener('click', () => {
-
-//         //popUpComment
-//     } );
-// })
 async function getItems(){
   const mealsData = await getMeals();
-  console.log(mealsData);
   mealsData.forEach((item) => {
+    let itemImage = item.strCategoryThumb;
+    let itemTitle = item.strCategory;
+    let itemDescription = item.strCategoryDescription;
+    console.log(item);
     let foodDetails= document.createElement('div');
     foodDetails.className = 'col-4 foodContent';    
     const onClickMeal = document.createElement('a');
@@ -23,9 +16,9 @@ async function getItems(){
     const mealTitle = document.createElement('h5');
     const mealDescription = document.createElement('p');
     onClickMeal.href="./foodRecipe.html";
-    mealImage.src='https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg'
-    mealTitle.innerHTML = ``;
-    mealDescription.innerHTML = ``;
+    mealImage.src=itemImage;
+    mealTitle.innerHTML = itemTitle;
+    mealDescription.innerHTML = itemDescription;
     onClickMeal.appendChild(mealImage);
     onClickMeal.appendChild(mealTitle);
     foodDetails.appendChild(onClickMeal);
