052db2917ffcbdff22953813c00b6efe53a3d66f
Buyaki <rittahbuyaki@gmail.com>
2021-12-30 16:11:16 +0300

    Add structure to the meals website

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+// function foodPopUp(){
+
+// }
+// const foodDetails = document.querySelectorAll('.foodContent')
+// foodDetails.forEach((item)=> {
+//     item.addEventListener('click', () => {
+
+//         //popUpComment
+//     } );
+// })
+
