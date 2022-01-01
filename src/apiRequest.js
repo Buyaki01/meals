81243ba663de90335a579eb80c11e3bb09d3134e
Buyaki <rittahbuyaki@gmail.com>
2022-01-01 17:30:24 +0300

    Add event listener on the image

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,5 +1,4 @@
 const requestMealsUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
-
 const getMealsFromApi = async () => {
   const data = await fetch(requestMealsUrl);
   const response = await data.json();
--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ async function getMeals(){
     const mealImage = document.createElement('img');
     const mealTitle = document.createElement('h5');
     // const mealDescription = document.createElement('p');
-    //onClickMeal.addEventListener()
+    onClickMeal.addEventListener('click', () => {
+      popUp(item);
+    });
     mealImage.src=itemImage;
     mealTitle.innerHTML = itemTitle;
     // mealDescription.innerHTML = itemDescription;
@@ -29,24 +31,21 @@ async function getMeals(){
     foodsBody.appendChild(foodDetails);
   })
 }
-async function popUp(){
-  const popUpForMeals = await getMealsFromApi();
-  popUpForMeals.forEach((item) => {
-    let itemImage = item.strMealThumb;
-    let itemTitle = item.strMeal;
-    let popUpMeals= document.createElement('div');
-    popUpMeals.className = 'mealsPopUp';
-    const popUpImage = document.createElement('img');
-    const popUpTitle = document.createElement('h3');
-    // const popUpRecipe = document.createElement('p');
-    popUpImage.src=itemImage;
-    popUpTitle.innerHTML = itemTitle;
-    popUpMeals.appendChild(popUpImage);
-    popUpMeals.appendChild(popUpTitle);
-    popUpC.appendChild(popUpMeals);
-    // popUpD.appendChild(popUpC);
-    // popUpM.appendChild(popUpD);
-  })
+async function popUp(item){
+  let itemImage = item.strMealThumb;
+  let itemTitle = item.strMeal;
+  let popUpMeals= document.createElement('div');
+  popUpMeals.innerHTML = ``;
+  popUpMeals.className = 'mealsPopUp';
+  const popUpImage = document.createElement('img');
+  const popUpTitle = document.createElement('h3');
+  // const popUpRecipe = document.createElement('p');
+  popUpImage.src=itemImage;
+  popUpTitle.innerHTML = itemTitle;
+  popUpMeals.appendChild(popUpImage);
+  popUpMeals.appendChild(popUpTitle);
+  popUpC.appendChild(popUpMeals);
+  // popUpD.appendChild(popUpC);
+  // popUpM.appendChild(popUpD);
 }
 getMeals();
-popUp();
