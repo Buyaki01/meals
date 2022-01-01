65b10bafdb4023202b5fc8f63af3fa02d3b2cb13
Buyaki <rittahbuyaki@gmail.com>
2022-01-01 18:32:24 +0300

    Get meal instruction/recipe from API using a different URL

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,7 +1,14 @@
-const requestMealsUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
+const requestMealsUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+const requestRecipeUrl = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const getMealsFromApi = async () => {
   const data = await fetch(requestMealsUrl);
   const response = await data.json();
   return response.meals;  
 };
-export {getMealsFromApi};
+
+const getRecipeFromApi = async (id) => {
+  const data = await fetch(`${requestRecipeUrl}${id}`);
+  const response = await data.json();
+  return response.meals[0];
+}
+export {getMealsFromApi, getRecipeFromApi};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css'
-import {getMealsFromApi} from './apiRequest.js';
+import {getMealsFromApi,getRecipeFromApi} from './apiRequest.js';
 
 const foodsBody= document.querySelector('.foods');
 const popUpM = document.querySelector('.popUpModal');
@@ -11,41 +11,45 @@ async function getMeals(){
   mealsData.forEach((item) => {
     let itemImage = item.strMealThumb;
     let itemTitle = item.strMeal;
-    // let itemDescription = item.strDescription; 
     let foodDetails= document.createElement('div');
     foodDetails.className = 'col-4 foodContent';    
     const onClickMeal = document.createElement('div');
     const mealImage = document.createElement('img');
     const mealTitle = document.createElement('h5');
-    // const mealDescription = document.createElement('p');
+    const seeRecipe = document.createElement('button');
+    seeRecipe.className ='btn btn-info btn-lg mb-2'
+    seeRecipe.dataset.toggle = 'modal';
+    seeRecipe.dataset.target = '#myModal';
     onClickMeal.addEventListener('click', () => {
       popUp(item);
     });
     mealImage.src=itemImage;
     mealTitle.innerHTML = itemTitle;
-    // mealDescription.innerHTML = itemDescription;
+    seeRecipe.innerHTML = `See recipe`;
     onClickMeal.appendChild(mealImage);
     onClickMeal.appendChild(mealTitle);
+    onClickMeal.appendChild(seeRecipe);
     foodDetails.appendChild(onClickMeal);
-    // foodDetails.appendChild(mealDescription);
     foodsBody.appendChild(foodDetails);
   })
 }
 async function popUp(item){
+  let recipe = await getRecipeFromApi(item.idMeal)
   let itemImage = item.strMealThumb;
   let itemTitle = item.strMeal;
+  let procedure = await recipe.strInstructions;
   let popUpMeals= document.createElement('div');
   popUpMeals.innerHTML = ``;
   popUpMeals.className = 'mealsPopUp';
   const popUpImage = document.createElement('img');
   const popUpTitle = document.createElement('h3');
-  // const popUpRecipe = document.createElement('p');
+  const popUpRecipe = document.createElement('p');
   popUpImage.src=itemImage;
   popUpTitle.innerHTML = itemTitle;
+  popUpRecipe.innerHTML = procedure;
   popUpMeals.appendChild(popUpImage);
   popUpMeals.appendChild(popUpTitle);
+  popUpMeals.appendChild(popUpRecipe);
   popUpC.appendChild(popUpMeals);
-  // popUpD.appendChild(popUpC);
-  // popUpM.appendChild(popUpD);
 }
 getMeals();
